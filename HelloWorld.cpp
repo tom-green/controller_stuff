18ba2d9221b89d23c404a434d863ed0914d62499
Thomas Green <tgreen@frontierwind.com>
2013-06-20 21:42:54 -0700

    Made some minor modifications to play with git a bit. 	o Added a couple of comments 	o Added an extra line of output

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,7 +6,10 @@
 
 int main()
 {
+	// Just adding some comments to change the file to "test" git
+	// Maybe add some more output...
 	printf("Hello World!\n\n");
+	printf("This is just a demo file to get the hang of git.\n\n");
 	return 0;
 }
 
